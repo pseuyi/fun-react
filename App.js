f8b0badfdd990b0b88161343799c51ad68e6fe4f
pseuyi <pseuyi@gmail.com>
2020-02-07 20:32:56 -0500

    revert some names, get props to work

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import {createElement as e} from './lib/react.js';
 
-const App = e('div', null, [
-  e('h1', null, ['header']),
-  e('button', null, ['button']),
-  e('marquee', null, ['do u like my app']),
-]);
+const header = ({title}) => e('h1', null, `name of app: ${title}`);
+
+const App = e(
+  'div',
+  null,
+  e(header, {title: 'hello world'}, ''),
+  e('button', null, 'button'),
+  e('marquee', null, 'do u like my app'),
+);
 
 export default App;
+
+/*
+const FnApp = () => {
+  return (
+    <div>
+      <h1></h1>
+      <button></button>
+      <marquee></marquee>
+    </div>
+  );
+};
+*/
